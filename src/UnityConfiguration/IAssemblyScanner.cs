659458e583d2a43184bbb8e6209012b496e34aba
thedersen <thedersen@gmail.com>
2011-03-19 18:17:55 +0100

    Documented the assembly scanner

--- src/UnityConfiguration/IAssemblyScanner.cs
+++ src/UnityConfiguration/IAssemblyScanner.cs
@@ -5,22 +5,76 @@ namespace UnityConfiguration
 {
     public interface IAssemblyScanner
     {
+        /// <summary>
+        /// Add an assembly to scan.
+        /// </summary>
+        /// <param name="assembly">The assembly to scan.</param>
         void Assembly(Assembly assembly);
+        /// <summary>
+        /// Add an assembly to scan given its display name.
+        /// </summary>
+        /// <param name="assemblyName">The display name of the assembly to scan (e.g. UnityConfiguration).</param>
         void Assembly(string assemblyName);
+        /// <summary>
+        /// Add an assembly to scan by specifying a type it contains.
+        /// </summary>
+        /// <typeparam name="T">A type that resides in the assembly to scan.</typeparam>
         void AssemblyContaining<T>();
+        /// <summary>
+        /// Add all assemblies in the application base directory of the current app domain to the scanner.
+        /// </summary>
         void AssembliesInBaseDirectory();
+        /// <summary>
+        /// Add all assemblies in the specified path to the scanner.
+        /// </summary>
+        /// <param name="path">The path to scan for assemblies.</param>
         void AssembliesInDirectory(string path);
-
+        /// <summary>
+        /// Add a convention to use when scanning.
+        /// </summary>
+        /// <typeparam name="TConvention">The type of the convention.</typeparam>
+        /// <returns>An instance of the convention that can be used for configuring the convention.</returns>
         TConvention With<TConvention>() where TConvention : IAssemblyScannerConvention, new();
+        /// <summary>
+        /// Adds a convention that will look for and include all <see cref="IUnityRegistry"/> that it finds.
+        /// </summary>
         void ForRegistries();
-
+        /// <summary>
+        /// Exclude the specified type when scanning.
+        /// </summary>
+        /// <typeparam name="T">Type of which to exclude.</typeparam>
         void ExcludeType<T>();
+        /// <summary>
+        /// Exclude types that match the specified predicate when scanning. 
+        /// </summary>
+        /// <param name="exclude">The predicate to use for matching.</param>
         void Exclude(Func<Type, bool> exclude);
+        /// <summary>
+        /// Exclude all types in the same namespace as the specified type or its sub namespaces
+        /// when scanning.
+        /// </summary>
+        /// <typeparam name="T">A type in the namespace to exclude.</typeparam>
         void ExcludeNamespaceContaining<T>();
+        /// <summary>
+        /// Exclude all types in the specified namespace or its sub namespaces when scanning.
+        /// </summary>
+        /// <param name="namespace">The namespace to exclude.</param>
         void ExcludeNamespace(string @namespace);
+        /// <summary>
+        /// Only include types that match the specified predicate when scanning.
+        /// </summary>
+        /// <param name="include">The predicate to use for matching.</param>
         void Include(Func<Type, bool> include);
+        /// <summary>
+        /// Only include types in the same namespace as the specified type or its sub namespaces
+        /// when scanning.
+        /// </summary>
+        /// <typeparam name="T">A type in the namespace to include.</typeparam>
         void IncludeNamespaceContaining<T>();
+        /// <summary>
+        /// Only include types in the specified namespace or its sub namespaces when scanning.
+        /// </summary>
+        /// <param name="namespace">The namespace to include.</param>
         void IncludeNamespace(string @namespace);
-        
     }
 }
--- src/UnityConfiguration/IAssemblyScannerConvention.cs
+++ src/UnityConfiguration/IAssemblyScannerConvention.cs
@@ -2,8 +2,20 @@
 
 namespace UnityConfiguration
 {
+    /// <summary>
+    /// Interface implemented by all scanner conventions. 
+    /// Defines a method to process each type the scanner finds.
+    /// </summary>
     public interface IAssemblyScannerConvention
     {
+        /// <summary>
+        /// Gets called for each type the <see cref="IAssemblyScanner"/> finds.
+        /// </summary>
+        /// <param name="type">The type to process.</param>
+        /// <param name="registry">
+        /// The instance of the <see cref="IUnityRegistry"/> 
+        /// that can be used to register the type.
+        /// </param>
         void Process(Type type, IUnityRegistry registry);
     }
 }
