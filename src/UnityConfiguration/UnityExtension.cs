4b46ebe57286d0f45bdf19eb1ba32b8a200a921b
thedersen <thedersen@gmail.com>
2011-03-19 23:18:09 +0100

    More documentation

--- src/UnityConfiguration/UnityExtension.cs
+++ src/UnityConfiguration/UnityExtension.cs
@@ -5,6 +5,11 @@ namespace UnityConfiguration
 {
     public static class UnityExtension
     {
+        /// <summary>
+        /// Configure an instance of an <see cref="IUnityContainer"/>.
+        /// </summary>
+        /// <param name="container">The container to configure.</param>
+        /// <param name="expression">An expression used for configuring the container.</param>
         public static void Configure(this IUnityContainer container, Action<IUnityRegistry> expression)
         {
             var registry = new UnityRegistry();
