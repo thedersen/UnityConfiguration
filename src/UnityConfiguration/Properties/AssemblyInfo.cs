b9f05022dc1d7cf00e95a958dae5273e4cc6caf7
thedersen <thedersen@gmail.com>
2011-12-18 19:40:17 +0100

    Updated AssemblyInfo with updated version number

--- src/UnityConfiguration/Properties/AssemblyInfo.cs
+++ src/UnityConfiguration/Properties/AssemblyInfo.cs
@@ -1,10 +1,10 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("UnityConfiguration")]
-[assembly: AssemblyDescription("Convention based configuration API for the Microsoft Unity IoC container.")]
+[assembly: AssemblyDescription("Convention based configuration API for the Microsoft Unity IoC container. With just a few lines of code, you can now registere all your classes in the entire solution. If the built-in conventions doesn't fit your needs, it is very easy to extend with your own.")]
 [assembly: AssemblyCompany("UnityConfiguration")]
 [assembly: AssemblyProduct("UnityConfiguration")]
 [assembly: AssemblyCopyright("Copyright (C) 2011 Thomas Pedersen")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.4.0")]
+[assembly: AssemblyVersion("1.4.1")]
 
